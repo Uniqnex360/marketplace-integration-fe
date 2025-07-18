3ee8d29f5c0418f64d43f56a91246e6577552169
Harisankar <harisankar@uniqnex360.com>
2025-07-18 15:44:07 +0530

    AllMarketPlace:Updated

--- src/Components/Client-Admin/Dashboard/AllMarketplace/AllMarketplace.js
+++ src/Components/Client-Admin/Dashboard/AllMarketplace/AllMarketplace.js
@@ -624,7 +624,7 @@ export default function AllMarketplace({
           </Grid>
 
           {/* Right side - CardComponent instead of Orders Chart */}
-          <Grid item xs={12} md={8} sx={{ ml: 2 }}>
+          <Grid item xs={12} md={8}>
               <CardComponent
                 widgetData={widgetData}
                 marketPlaceId={marketPlaceId}
