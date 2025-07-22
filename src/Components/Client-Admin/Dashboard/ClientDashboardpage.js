704f4755d2217e12a61ea4139db79b71e9bb9362
Harisankar <harisankar705@gmail.com>
2025-07-22 13:09:51 +0530

    MetricCard:Updated

--- src/Components/Client-Admin/Dashboard/ClientDashboardpage.js
+++ src/Components/Client-Admin/Dashboard/ClientDashboardpage.js
@@ -1901,7 +1901,7 @@ function ClientDashboardpage() {
               selectedCategory === "all" ? selectedCategory : filterFinal
             }
             brand_id={selectedBrandFilter}
-            product_id={mergedProductsFilter?.id}
+            product_id={mergedProductsFilter}
             manufacturer_name={selectedManufacturerFilter}
             fulfillment_channel={selectedFulfillment}
             DateStartDate={appliedStartDate}
