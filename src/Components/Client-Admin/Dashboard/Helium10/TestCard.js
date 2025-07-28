a5a5c0dbefecb7606ea71cd8c7a7d1d6d010f239
Harisankar <harisankar705@gmail.com>
2025-07-28 14:35:09 +0530

    TestCard:Updated

--- src/Components/Client-Admin/Dashboard/Helium10/TestCard.js
+++ src/Components/Client-Admin/Dashboard/Helium10/TestCard.js
@@ -372,9 +372,11 @@ const TestCard = ({
           newSelectedDate = today.endOf("week");
           break;
         case "Last Week":
-          newDisplayDate =today.startOf("week").subtract(1, "week");
-          newSelectedDate =today.startOf("week").subtract(1, "week").endOf("week");
-          break;
+  newDisplayDate = today.clone().subtract(1, 'week').startOf('week');
+  newSelectedDate = today.clone().subtract(1, 'week').endOf('week');
+  break;
+          
+
 
         case "Last 7 days":
           newDisplayDate = today.subtract(6, "day");
